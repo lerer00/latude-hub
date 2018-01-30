1147032b25520bb2caa6abebd7a12c28abef2262
Francis Boily <boilyfrank28@gmail.com>
2018-01-29 22:21:29 -0500

    Property model was updated

---
+++ app/models/comment.js
@@ -0,0 +1,9 @@
+var Comment = class Comment {
+    constructor() {
+        this.id = '';
+        this.owner = '';
+        this.text = '';
+    }
+}
+
+module.exports = Comment;
--- app/models/property.js
+++ app/models/property.js
@@ -1,9 +1,13 @@
 var Location = require('./location');
+var Comment = require('./comment');
 
 var Property = class Property {
     constructor() {
         this.id = '';
         this.name = '';
+        this.description = '';
+        this.rating = 0;
+        this.comments = []
         this.active = false;
         this.parent = '';
         this.location = new Location();
