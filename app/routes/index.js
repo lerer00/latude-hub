33675dd3ad09305d351cee43450126fdeb4437ac
Francis Boily <boilyfrank28@gmail.com>
2018-02-13 23:45:45 -0500

    Adding fix for preflight call when using /authenticate endpoint.

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,7 @@ const authenticateMiddleware = require('../middleware/authenticateMiddleware');
 module.exports = function (app) {
   // anonymous calls
   app.route('/').get(rootController.api_greeter);
+  app.route('/authenticate').post(authenticationController.post_authenticate);
   app.route('/companies').get(companiesController.get_companies);
   app.route('/companies/:id').get(companiesController.get_company);
   app.route('/properties').get(propertiesController.get_properties);
@@ -19,7 +20,6 @@ module.exports = function (app) {
   app.route('/assets').get(assetsController.get_assets);
   app.route('/assets/:id').get(assetsController.get_asset);
   app.route('/availabilities').get(availabilitiesController.get_availabilities);
-  app.route('/authenticate').post(authenticationController.post_authenticate);
 
   // authorized calls
   app.use(authenticateMiddleware.authenticate);
--- server.js
+++ server.js
@@ -18,7 +18,16 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+    //intercepts OPTIONS method
+    if (req.method === 'OPTIONS') {
+        //respond with 200
+        res.status(200).send({});
+    }
+    else {
+        //move on
+        next();
+    }
 });
 
 // populate cosmosDb with dev data
